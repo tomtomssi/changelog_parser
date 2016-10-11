46a45b05410d66d31e2082a3f4e33ca09dc1fede
Tatu Solla <tatu.solla@gmail.com>
2016-10-11 10:43:39 +0300

    Add some tests

--- test/changelog_parser.js
+++ test/changelog_parser.js
@@ -4,9 +4,23 @@ var fs = require('fs');
 var expect = require('chai').expect;
 
 describe('#parser', function () {
-  it('TESTLOG should have 60 lines', function () {
-    var log = fs.readFileSync('./test/TESTLOG.md', 'utf8').split('\n');
+  var changelogPath = './test/TESTLOG.md';
+  var json_changelog;
 
+  beforeEach(function () {
+    json_changelog = parser.toJSON(changelogPath);
+  });
+  
+  it('TESTLOG should have 60 lines', function () {
+    log = fs.readFileSync('./test/TESTLOG.md', 'utf8').split('\n');    
     log.length.should.equal(60);
+  });
+
+  it('json_changelog should be defined', function () {
+    json_changelog.should.be.ok;
+  });
+
+  it('json_changelog should have 5 versions', function () {
+    json_changelog.versions.length.should.equal(5);
   });
 });
